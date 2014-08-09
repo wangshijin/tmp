92c494482ccc099bb9c6a3dc0a176594a95f2cbb
wangshijin <wangshijin314@163.com>
2014-08-09 22:08:33 +0800

    modify websocket_example

--- websocket_example/websocket.go
+++ websocket_example/websocket.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
+	"io/ioutil"
 	"log"
 	"time"
 )
 
 func Echo(ws *websocket.Conn) {
-	var err error
-
-	count := 0
+	filename := "echo.txt"
 	for {
 		<-time.After(time.Second * 1)
-		count++
-		if err = websocket.Message.Send(ws, count); err != nil {
+
+		buf, err := ioutil.ReadFile(filename)
+		if err != nil {
+			continue
+		}
+
+		if err = websocket.Message.Send(ws, string(buf)); err != nil {
 			log.Println("Can't send")
 			break
 		}
